a321b81fa4eedae3c5c2110d5255042900f5bf24
rdarj <rdarji4040@gmail.com>
2022-09-28 14:53:48 -0400

    umnessary methods are remove

--- Laptop.java
+++ Laptop.java
@@ -3,16 +3,5 @@ public class Laptop {
     public void addNumber(int a , int b){
         return a+b;
     }
-    public void NEWaDD(int a ){
-        return a+b;
-    }
-      public void ADD345(int a , int b){
-        return a+b;
-    }
-    public void addNumber(int a , int b){
-        return a+b;
-    }
-    public void addNumber(int a , int b){
-        return a+b;
-    }
+
 }
