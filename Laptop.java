df0f4cbd56487ca0055538e9e7c1725811c24a82
rdarj <rdarji4040@gmail.com>
2022-09-28 13:50:58 -0400

    add metood named addNumber

--- Laptop.java
+++ Laptop.java
@@ -3,6 +3,9 @@ public class Laptop {
     public void addNumber(int a , int b){
         return a+b;
     }
+    public void NEWaDD(int a ){
+        return a+b;
+    }
     public void addNumber(int a , int b){
         return a+b;
     }
