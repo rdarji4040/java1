b9eac558c5624239cede0a05478390c6eb5651a5
rdarji4040 <114437614+rdarji4040@users.noreply.github.com>
2022-09-28 13:48:49 -0400

    Update Laptop.java

--- Laptop.java
+++ Laptop.java
@@ -3,4 +3,13 @@ public class Laptop {
     public void addNumber(int a , int b){
         return a+b;
     }
+     public void ADD( int b){
+        return a+b;
+    }
+     public void ADD(int a , int b){
+        return a+b;
+    }
+     public void ADD(int a , int b){
+        return a+b;
+    }
 }
