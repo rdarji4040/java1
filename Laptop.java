caf2a059c33fd54ab329cf768346b68ec34e19d3
rdarj <rdarji4040@gmail.com>
2022-09-28 15:30:31 -0400

    method created

--- Laptop.java
+++ Laptop.java
@@ -1,8 +1,12 @@
 public class Laptop {
+    public String name(){
+        return "its rohit";
+    }
 
     public void addNumber(int a , int b){
         return a+b;
     }
+
 
     public void added(int a ){
         return a+b;
