5bd85ebecd169d7dce080e1d56e248bb62dc4953
Rohit Darji <rdarji4040@gmail.com>
2022-09-28 12:37:33 -0400

    new

---
+++ index.js
@@ -0,0 +1,19 @@
+var a = 5;
+var b = "divas regmi";
+var c = 4.5;
+
+console.log("hello i am here");
+const element = document.getElementById("text1");
+
+const btn = document.getElementById("btn")
+
+btn.onclick((clicck)=>{
+    
+})
+
+function increase() {
+    element.innerHTML = a++
+    
+}
+
+j
